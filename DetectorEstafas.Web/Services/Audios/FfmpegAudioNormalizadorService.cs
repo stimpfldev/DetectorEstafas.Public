b4de22c5e1bbe3ace01c0fda09d0028539c17ef6
Federico Stimpfl <stimpfldev@gmail.com>
2026-08-30 20:42:21 -0300

    Replace FFmpeg normalizer on shared hosting

--- DetectorEstafas.Web/Services/Audios/FfmpegAudioNormalizadorService.cs
+++ DetectorEstafas.Web/Services/Audios/FfmpegAudioNormalizadorService.cs
@@ -1,32 +1,14 @@
-﻿using FFMpegCore;
-
 namespace DetectorEstafas.Web.Services.Audios;
 
 public class FfmpegAudioNormalizadorService
     : IAudioNormalizadorService
 {
-    public async Task NormalizarAWavAsync(
+    public Task NormalizarAWavAsync(
         string rutaOrigen,
         string rutaDestino,
         CancellationToken cancellationToken)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(rutaOrigen);
-        ArgumentException.ThrowIfNullOrWhiteSpace(rutaDestino);
-
-        if (!File.Exists(rutaOrigen))
-        {
-            throw new FileNotFoundException(
-                "No se encontró el audio a normalizar.",
-                rutaOrigen);
-        }
-
-        await FFMpegArguments
-            .FromFileInput(rutaOrigen)
-            .OutputToFile(
-                rutaDestino,
-                overwrite: true,
-                options => options.WithCustomArgument(
-                    "-vn -ac 1 -ar 16000 -c:a pcm_s16le"))
-            .ProcessAsynchronously();
+        throw new NotSupportedException(
+            "La normalización de audio no está disponible en este entorno.");
     }
 }
